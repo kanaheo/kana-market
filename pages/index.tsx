0c5dec0eee9101530af66e46b88a207ccbf60c1b
heokyoungmin <stalvede0421@gmail.com>
2022-02-15 23:03:35 +0900

    kana market setting!

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-green-500">
+      <h1 className="text-center text-blue-600 text-lg">
+        TailWind가 작동할까?
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
